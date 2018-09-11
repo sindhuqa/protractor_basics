3f384591df61d4182160afde6d7437601987b616
Your Name <you@example.com>
2018-09-10 22:08:25 -0400

    Add bat file for execution of protractor files
    - Inside bat file give "Conf" file of perticular script and then run "bat" file
    - You can create "bat" file for every "Conf" file execution
    
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   command.bat

---
+++ command.bat
@@ -0,0 +1,5 @@
+protractor loggingConf.js
+
+/* If you are have "suites" in the config file, the in the bat file the comaand should be given like this */
+
+/* protractor config.js --suite functional,Regression */
